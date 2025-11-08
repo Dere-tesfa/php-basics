ea2460454e0665d49b35bd2fc3248635fd9341fa
dere <deretesfa6@gmail.com>
2025-11-07 23:49:25 -0800

    variables

---
+++ php__basic/Variable.php
@@ -0,0 +1,19 @@
+<?php
+/*
+- rules to define a variable in php
+1.A variable must start with a $ symbol,then its name;
+2.The varible name must start with a letter or underscore(_).
+3.A variable name cannot start with  a number.
+4.The variable name can contain letters,digitsor underscore.
+5.PHP is case sensitive,so $Name and $name are distinct variables
+
+*/
+$name="dereje";
+
+echo $name;
+$Name="Gebre";
+echo $Name;
+$age=22;
+
+
+?>
