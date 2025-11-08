b03b46703601daff14fcb1e96c7aba2ddd7f0de3
dere <deretesfa6@gmail.com>
2025-11-08 00:33:29 -0800

    Arithmetic_operators

---
+++ php__basic/operators/Arithmetic_operators.php
@@ -0,0 +1,18 @@
+<?php
+$x=30;
+$y=5;
+
+
+$value=$x+$y;// 35
+$value=$x-$y;// 25
+$value=$x/$y;// 6
+$value=$x%$y;// 0
+$value=$x*$y;// 150
+$value=$x**$y;// 24300000 $x raised to the power of $y
+
+
+
+
+
+
+?>
