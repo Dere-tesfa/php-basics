73dc280dcb020d465772c1d859b9fe7d3fd7887e
dere <deretesfa6@gmail.com>
2025-11-07 23:32:47 -0800

    echo

--- php__basic/test.php
+++ php__basic/test.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php</title>
-</head>
-<body>
+
     <?php
-echo "hello world coders"
-?>
-</body>
-</html>
+    echo "<h1>Hello World</h1>"
+    
+    ?>
+<h1>
+    <?php
+    echo "my first class"
+    ?>
+</h1>
