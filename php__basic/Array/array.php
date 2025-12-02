5809672b20eb632c5df1c9eb079c5bf62c1b405f
dere <deretesfa6@gmail.com>
2025-12-02 08:44:00 -0800

    Array_php

---
+++ php__basic/Array/array.php
@@ -0,0 +1,34 @@
+<?php
+//An array in PHP is a data structure that can hold multiple values under a single name.
+// Each value in the array can be accessed using its corresponding index or key.
+//  There are three types of arrays in PHP:
+// 1. Indexed Arrays: Arrays with a numeric index.
+// 2. Associative Arrays: Arrays with named keys.
+// 3. Multidimensional Arrays: Arrays containing one or more arrays.
+//1. Indexed Arrays
+
+$arr=array("Dereje","gebre","jone","bro");
+echo $arr[0]."</br>";
+echo $arr[1]."</br>";       
+echo $arr[2]."</br>";
+echo $arr[3]."</br>";
+$scores=[90,80,70,60];
+echo $scores [0]."</br>";
+echo $scores [1]."</br>";
+echo $scores [2]."</br>";
+
+//2. Associative Arrays
+// Associative arrays use named keys that you assign to them.
+// Example:
+
+$age=array("Dereje"=>25,"gebre"=>30,"jone"=>28);
+echo $age["Dereje"]."</br>";
+echo $age["gebre"]."</br>";
+echo $age["jone"]."</br>";
+// example:2
+// Using the shorthand array syntax
+
+$products=["Labtop"=>15000,"memory"=>250,"monitor"=>450000];
+var_dump($products);
+echo "</br>";
+?>
--- php__basic/operators/Arithmetic_operators.php
+++ php__basic/operators/Arithmetic_operators.php
@@ -9,8 +9,9 @@ $value=$x/$y;// 6
 $value=$x%$y;// 0
 $value=$x*$y;// 150
 $value=$x**$y;// 24300000 $x raised to the power of $y
-$x++// 30
-++$x//32
-$x--//32
---$x//30
+// $x++// 30
+// // ++$x//32
+// $x--//32
+// --$x//30
+echo $value;
 ?>
